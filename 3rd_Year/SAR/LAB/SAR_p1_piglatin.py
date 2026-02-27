2ae7c1fa2838dd7c37eefd67bd04197aa836f346
AdriaMB <amarbay@etsinf.upv.es>
2026-02-27 20:01:40 +0100

    SAR_Piglatin changes

--- 3rd_Year/SAR/LAB/SAR_p1_piglatin.py
+++ 3rd_Year/SAR/LAB/SAR_p1_piglatin.py
@@ -1,3 +1,9 @@
+# Integrantes del equipo:
+#   Adrià Marín Bayarri
+#   Blanca Verdeguer Tomás
+#   Sergio Quintanilla Graciá
+
+
 #!/usr/bin/env python
 #! -*- encoding: utf8 -*-
 
@@ -22,10 +28,6 @@ class Translator():
             punt = ".,;?!"
         self.re = re.compile(r"(\w+)([" + punt + r"]*)")
 
-
-
-
-
     def translate_word(self, word:Text) -> Text:
         """
         Recibe una palabra en inglés y la traduce a Pig Latin
@@ -33,42 +35,48 @@ class Translator():
         :param word: la palabra que se debe pasar a Pig Latin
         :return: la palabra traducida
         """
+
+        punctuations = ('.', ',', ';', '?', '!')
         
+        # Store the punctuation character at the end of the word if it exists
+        punctuation = ''
+        if word[-1] in punctuations:
+            punctuation = word[-1]
+            word = word[:-1]
 
-        capitalize = word[0].isupper() and word[1].islower()
+        # If the word contains numbers or characters that are not letters, don't
+        # process it
+        if not word.isalpha():
+            return word + punctuation
 
-        vowels = "aeiouyAEIOUY"
+        vowels = ('y', 'a', 'e', 'i', 'o', 'u')
+        is_first_uppercase = word[0].isupper()
+        is_all_uppercase = is_first_uppercase and word[-1].isupper()
 
-        new_word = word
-        # The word contains a number or other non-alphabetic character
-        if not word[0].isalpha():
-            return new_word 
+        # We process the word in uppercase to simplify the process
+        word = word.lower()
 
-        # else, if the word starts by a vowel...
-        elif word[0] in vowels:
-            new_word += "YAY" if word.isupper() else "yay" # OLA -> OLAYAY    ola -> olayay    Ola -> Olayay
-        # else, the word stars by a consonant
+        if word[0] in vowels:
+            new_word = word + 'yay' + punctuation
+
         else:
-            first_vowel = null    # default index (En python los índices pueden ser negativos sin que eso rompa el programa: simplemente comienza por atrás... Por esta razón, mejor dejar first_vowel como null y no como un valor negativo)
-            for i in word:
-                if i in vowels:
-                    first_vowel = word.find(i)      # Store the index of the first vowel
+            # Find the first index of a vowel
+            first_vowel_i = 0
+            for i in range(len(word)):  # We could also use enumerate
+                if word[i] in vowels:
+                    first_vowel_i = i
                     break
-            if first_vowel != 0:                    # Technically, is always going to be different to 0. If there was a vowel in the first position, we wouldn't be here...'
-                new_word = word[first_vowel:] + word[:first_vowel] + "ay"
-                new_word = new_word.lower()
-            else:
-                new_word = word
+            
+            new_word = word[first_vowel_i:] + word[:first_vowel_i] + 'ay' + punctuation
+        
+        # Restore capitalization
+        if is_all_uppercase:
+            new_word = new_word.upper()
 
-        if(word.isupper()): new_word = new_word.upper()             # If the original word was UPPER, convert it
-        elif(capitalize): new_word = new_word.lower().capitalize()  # If the original word had the first letter in capital form, convert it
+        elif is_first_uppercase:
+            new_word = new_word.capitalize()
 
         return new_word
-
-
-
-
-
 
     def translate_sentence(self, sentence:Text) -> Text:
         """
@@ -78,40 +86,10 @@ class Translator():
         :return: la frase traducida
         """
 
-        new_sentence = "" # SUSTITUIR ESTA PARTE
-        for word in sentence.split(" "):                        # We separate the words of the line by the " "
-                                                                # Now, we filter the punctuation marks that the resulting words might have
-            #If the word ends in a comma, keep it for later
-            if word[len(word)-1] == ",":
-                word = word.replace(",", "")
-                new_sentence += self.translate_word(word) + ", "
-            # If the word ends in a point, same
-            elif word[len(word)-1] == ".":
-                word = word.replace(".", "")
-                new_sentence += self.translate_word(word) + ". "
-            # Same with ;
-            elif word[len(word)-1] == ";":
-                word = word.replace(";", "")
-                new_sentence += self.translate_word(word) + "; "
-            #
-            elif word[len(word)-1] == "?":
-                word = word.replace("?", "")
-                new_sentence += self.translate_word(word) + "? "
-            #
-            elif word[len(word)-1] == "!":
-                word = word.replace("!", "")
-                new_sentence += self.translate_word(word) + "! "
-            # Else, if the word doesn't have any punctiation point...
-            else:
-                new_sentence += self.translate_word(word) + " "
-        
+        new_sentence_arr = [self.translate_word(word) for word in sentence.split(' ')]
+        new_sentence = ' '.join(new_sentence_arr)
+
         return new_sentence
-
-        """ In this commented version, the punctuation symbols are handled in the translate_word method """
-        # aux = [self.translate_word(word) for word in sentence .split(' ')]
-        # new_sentence = ' '.join(aux)
-
-
 
     def translate_file(self, filename:Text):
         """
@@ -124,16 +102,6 @@ class Translator():
         if not isfile(filename):
             print(f'{filename} no existe o no es un nombre de fichero', file=sys.stderr)
 
-        with (
-            open(filename, "r", encoding="utf-8") as f,
-            open("new_file.txt", "w", encoding="utf-8") as new_file
-        ):
-            for line in f.readlines():
-                print(line)
-                aux = self.translate_sentence(line.replace("\n", ""))
-                new_file.write(aux + "\n")
-                
-        """
         with open(filename, 'r') as original:
             filename_arr = filename.split('.')
             new_filename = filename_arr[0] + '_latin' + '.' + filename_arr[1]
@@ -145,22 +113,14 @@ class Translator():
                 for line in original:
                     newfile.write(self.translate_sentence(line.strip()) + '\n')
 
-        """
-
-
-
-
-
-
-
 if __name__ == "__main__":
-    if len(sys.argv) > 2:                                       # Si hay demasiados argumentos
+    if len(sys.argv) > 2:
         print(f'Syntax: python {sys.argv[0]} [filename]')
         exit()
     t = Translator()
-    if len(sys.argv) == 2:                                      # Si Hay 1 argumento: el nombre del fichero de texto
+    if len(sys.argv) == 2:
         t.translate_file(sys.argv[1])
-    else:                                                       # Sino, entra en loop donde pida palabras que traducir
+    else:
         sentence = input("ENGLISH: ")
         while len(sentence) > 1:
             print("PIG LATIN:", t.translate_sentence(sentence))
---
+++ 3rd_Year/SAR/LAB/my_SAR_p1_piglatin.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+#! -*- encoding: utf8 -*-
+
+# 1.- Pig Latin
+
+import re
+import sys
+from typing import Optional, Text
+from os.path import isfile
+
+class Translator():
+
+    def __init__(self, punt:Optional[Text]=None):
+        """
+        Constructor de la clase Translator
+
+        :param punt(opcional): una cadena con los signos de puntuación
+                                que se deben respetar
+        :return: el objeto de tipo Translator
+        """
+        if punt is None:
+            punt = ".,;?!"
+        self.re = re.compile(r"(\w+)([" + punt + r"]*)")
+
+
+
+
+
+    def translate_word(self, word:Text) -> Text:
+        """
+        Recibe una palabra en inglés y la traduce a Pig Latin
+
+        :param word: la palabra que se debe pasar a Pig Latin
+        :return: la palabra traducida
+        """
+        
+
+        capitalize = word[0].isupper() and word[1].islower()
+
+        vowels = "aeiouyAEIOUY"
+
+        new_word = word
+        # The word contains a number or other non- character
+        if not word[0].isalpha():
+            return new_word 
+
+        # else, if the word starts by a vowel...
+        elif word[0] in vowels:
+            new_word += "YAY" if word.isupper() else "yay" # OLA -> OLAYAY    ola -> olayay    Ola -> Olayay
+        # else, the word stars by a consonant
+        else:
+            first_vowel = None    # default index (En python los índices pueden ser negativos sin que eso rompa el programa: simplemente comienza por atrás... Por esta razón, mejor dejar first_vowel como null y no como un valor negativo)
+            for i in word:
+                if i in vowels:
+                    first_vowel = word.find(i)      # Store the index of the first vowel
+                    break
+            if first_vowel != 0:                    # Technically, is always going to be different to 0. If there was a vowel in the first position, we wouldn't be here...'
+                new_word = word[first_vowel:] + word[:first_vowel] + "ay"
+                new_word = new_word.lower()
+            else:
+                new_word = word
+
+        if(word.isupper()): new_word = new_word.upper()             # If the original word was UPPER, convert it
+        elif(capitalize): new_word = new_word.lower().capitalize()  # If the original word had the first letter in capital form, convert it
+
+        return new_word
+
+
+
+
+
+
+    def translate_sentence(self, sentence:Text) -> Text:
+        """
+        Recibe una frase en inglés y la traduce a Pig Latin
+
+        :param sentence: la frase que se debe pasar a Pig Latin
+        :return: la frase traducida
+        """
+
+        new_sentence = "" # SUSTITUIR ESTA PARTE
+        for word in sentence.split(" "):                        # We separate the words of the line by the " "
+                                                                # Now, we filter the punctuation marks that the resulting words might have
+            #If the word ends in a comma, keep it for later
+            if word[len(word)-1] == ",":
+                word = word.replace(",", "")
+                new_sentence += self.translate_word(word) + ", "
+            # If the word ends in a point, same
+            elif word[len(word)-1] == ".":
+                word = word.replace(".", "")
+                new_sentence += self.translate_word(word) + ". "
+            # Same with ;
+            elif word[len(word)-1] == ";":
+                word = word.replace(";", "")
+                new_sentence += self.translate_word(word) + "; "
+            #
+            elif word[len(word)-1] == "?":
+                word = word.replace("?", "")
+                new_sentence += self.translate_word(word) + "? "
+            #
+            elif word[len(word)-1] == "!":
+                word = word.replace("!", "")
+                new_sentence += self.translate_word(word) + "! "
+            # Else, if the word doesn't have any punctiation point...
+            else:
+                new_sentence += self.translate_word(word) + " "
+        
+        return new_sentence
+
+        """ In this commented version, the punctuation symbols are handled in the translate_word method """
+        # aux = [self.translate_word(word) for word in sentence .split(' ')]
+        # new_sentence = ' '.join(aux)
+
+
+
+    def translate_file(self, filename:Text):
+        """
+        Recibe un fichero y crea otro con su tradución a Pig Latin
+
+        :param filename: el nombre del fichero que se debe traducir
+        :return: None
+        """
+        
+        if not isfile(filename):
+            print(f'{filename} no existe o no es un nombre de fichero', file=sys.stderr)
+
+        name = filename.split(".")
+        print(name)
+
+        with (
+            open(filename, "r", encoding="utf-8") as f,
+            open(name[0] +"_latin.txt", "w", encoding="utf-8") as new_file
+        ):
+            for line in f.readlines():
+                
+                aux = self.translate_sentence(line.replace("\n", ""))
+                new_file.write(aux + "\n")
+                
+        """
+        with open(filename, 'r') as original:
+            filename_arr = filename.split('.')
+            new_filename = filename_arr[0] + '_latin' + '.' + filename_arr[1]
+
+            # Empty the file in case it already exists
+            open(new_filename, 'w').close()
+
+            with open(new_filename, 'a') as newfile:
+                for line in original:
+                    newfile.write(self.translate_sentence(line.strip()) + '\n')
+
+        """
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:                                       # Si hay demasiados argumentos
+        print(f'Syntax: python {sys.argv[0]} [filename]')
+        exit()
+    t = Translator()
+    if len(sys.argv) == 2:                                      # Si Hay 1 argumento: el nombre del fichero de texto
+        t.translate_file(sys.argv[1])
+    else:                                                       # Sino, entra en loop donde pida palabras que traducir
+        sentence = input("ENGLISH: ")
+        while len(sentence) > 1:
+            print("PIG LATIN:", t.translate_sentence(sentence))
+            sentence = input("ENGLISH: ")
